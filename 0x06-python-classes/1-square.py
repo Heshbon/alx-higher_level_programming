677c85260e78c9d41c3bca0c48909f490512b8db
Heshbon <cityalight.hesbon@gmail.com>
2023-12-20 13:04:36 -0800

    Python Square that defines a class square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a class square"""
+
+    def __init__(self, size):
+        """square initialization
+
+        Args:
+            size (int): size of the new square
+        """
+        self.__size = size
