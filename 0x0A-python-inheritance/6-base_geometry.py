911de7723528d5eefcdb63ebbd52eac212841e51
Heshbon <cityalight.hesbon@gmail.com>
2024-01-10 18:57:47 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines class BaseGeometry"""
+
+
+class BaseGeometry:
+    """represents BaseGeometry"""
+
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
