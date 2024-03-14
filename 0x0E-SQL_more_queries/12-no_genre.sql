fdefc06873025389f51995e8ea5735727a6dbd6a
Heshbon <cityalight.hesbon@gmail.com>
2024-03-13 18:36:33 -0700

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all shows contained in hbtn_0d_tvshows without a genre linked.
--- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
+-- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
        LEFT JOIN `tv_show_genres` AS g
