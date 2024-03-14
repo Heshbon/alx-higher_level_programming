7dbabf0a1a0b12f90acd534a253d4078d4a7e2c2
Heshbon <cityalight.hesbon@gmail.com>
2024-03-13 17:38:47 -0700

    10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
+SELECT show.`title`, genre.`genre_id`
+  FROM `tv_shows` AS show
+        INNER JOIN `tv_show_genres` AS genre
+	ON show.`id` = genre.`show_id`
+ ORDER BY show.`title`, genre.`genre_id`;
