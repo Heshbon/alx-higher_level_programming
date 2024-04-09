2a4572644b1d57e3f183f103837cea88bf0a4d34
Heshbon <cityalight.hesbon@gmail.com>
2024-04-09 15:10:36 -0700

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
+  return list.reduce((add_up, now) => now  === searchElement ? add_up + 1 : add_up, 0);
 };
