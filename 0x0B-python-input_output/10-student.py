41c63d39681d965ca8f4fdd51526269ea4fd058c
Heshbon <cityalight.hesbon@gmail.com>
2024-04-22 11:33:52 -0700

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-"""defines a student"""
+"""class Student that defines a student"""
 
 
 class Student:
-    """represents student"""
     def __init__(self, first_name, last_name, age):
+        """Initialisation of student object"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student"""
-        if attrs is None:
+        """If attrs is a list of strings, only attribute names contained
+        in this list must be retrieved otherwise retrieve all
+        """
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
             return self.__dict__
-        my_attrs = {}
-        for a, num in self.__dict__.items():
-            my_attrs[a] = num
-        return my_attrs
+
+        my_name = dict()
+
+        for key, a in self.__dict__.items():
+            if key in attrs:
+                my_name[key] = a
+        return my_name
